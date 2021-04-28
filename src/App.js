dfdb770cc995af3364e0ebb5d0790cc6ec69f82e
Aspodel <dattran2309@gmail.com>
2021-04-28 12:49:21 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import ReturnRequest from "./components/ReturnRequest";
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        padding:0;
+        margin:0;
+        box-sizing:border-box;
+    }
+`;
+
+const theme = {
+  primary: "#2B74E4",
+  stoke: "#828282",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <GlobalStyle />
+        <ReturnRequest />
+      </div>
+    </ThemeProvider>
   );
 }
 
